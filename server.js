3e8ae6bfb2145c75bda652e0ed9e0fe92a86db31
Vick Manjarrez <vick.manjarrez@gmail.com>
2020-11-13 01:29:20 -0600

    updating database name on server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const mongoose = require("mongoose");
 
 // Setting up Express App
 const app = express();
-const PORT = process.env.PORT || 8000;
 
 app.use(morgan("dev"));
 
@@ -16,7 +15,7 @@ app.use(express.static('public'));
 
 // db mongo
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker',
+    process.env.MONGODB_URI || 'mongodb://localhost/vicks-workout-tracker',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -30,6 +29,6 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 // Starts the server to begin listening
-app.listen(PORT, function(){
-    console.log(`App listening on Port ${PORT}!`);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
